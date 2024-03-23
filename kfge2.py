66365c5af20ae716dc96e8b51ad7df9fe4ecc5a9
amitd8 <amitdan8@gmail.com>
2024-03-23 13:15:14 +0200

    Created Masquarding detections and added .gitignore file to ignore outputs folder

--- kfge2.py
+++ kfge2.py
@@ -2,7 +2,7 @@
 # Map a dictionery full of usful process data for later anomaly analysis
 def enumeratePslist():
     pps = {}
-    with open("C:\\Users\\Blue\\Desktop\\PSlistAnalytics\\outputest.txt", "r") as file:
+    with open(".\\outputest.txt", "r") as file:
         # Iterate over each line in the file
         
         for line in file:
@@ -11,6 +11,7 @@ def enumeratePslist():
                 pps[a[0]] = [a[1],a[2]]
                     
     return pps
+#  pid : [ppid,imagename]
 
 def printhirerchyhelper(pid,apps):
     if pid in apps:
@@ -18,16 +19,97 @@ def printhirerchyhelper(pid,apps):
 def printhirerchy(pid,apps):
         return str(printhirerchyhelper(pid,apps)).replace("None --> ","")
 
-# def findlegit pids
+def findlegitpids():
+     print ("wow")
 
-# def for suspicous count
+def count_occurrences(apps, pname):
+    count = 0
+    pids = []
+    for pid, name in apps.items():
+        if name[1] == pname:
+            count += 1
+            pids.append(pid)
+    return count, pids
+
+def DetectMasquerading(apps):
+     analysis = ""
+     ps = ["lsass.exe","services.exe","wininit.exe","csrss.exe","RuntimeBroker.exe","taskhostw.exe","svchost.exe"]
+     for process in ps:
+        count, pids = count_occurrences(apps,process)
+        if process == "lsass.exe":
+            for pid in pids:
+                if apps[pid][0] not in apps:
+                    analysis += "   Rough lsass.exe process detected! Pid: "+pid+", None-Existing Parent: "+ apps[pid][0]+ "\n"
+                else:
+                    if apps[apps[pid][0]][1] != "wininit.exe":
+                        analysis += "   Rough lsass.exe process detected! Pid: "+pid+", abnormal Parent: "+ apps[apps[pid][0]][1]+ "\n"
+        elif process == "services.exe":
+            for pid in pids:
+                if apps[pid][0] not in apps:
+                    analysis += "   Rough services.exe process detected! Pid: "+pid+", None-Existing Parent: "+ apps[pid][0]+ "\n"
+                else:
+                    if apps[apps[pid][0]][1] != "wininit.exe":
+                        analysis += "   Rough services.exe process detected! Pid: "+pid+", abnormal Parent: "+ apps[apps[pid][0]][1]+ "\n"
+        elif process == "wininit.exe":
+            for pid in pids:
+                if apps[pid][0] in apps:
+                        analysis += "   Rough wininit.exe process detected! Pid: "+pid+", Wininit.exe usualy has an exited parent ,abnormal Parent: "+ apps[apps[pid][0]][1]+" ("+apps[pid][0]+")\n"
+        elif process == "csrss.exe":
+            for pid in pids:
+                if apps[pid][0] in apps:
+                        analysis += "   Rough csrss.exe process detected! Pid: "+pid+", csrss.exe usualy has an exited parent ,abnormal Parent: "+ apps[apps[pid][0]][1]+" ("+apps[pid][0]+")\n"
+        elif process == "svchost.exe":
+            for pid in pids:
+                if apps[pid][0] not in apps:
+                    analysis += "   Rough svchost.exe process detected! Pid: "+pid+", None-Existing Parent: "+ apps[pid][0]+ "\n"
+                else:
+                    if apps[apps[pid][0]][1] != "services.exe":
+                        analysis += "   Rough svchost.exe process detected! Pid: "+pid+", abnormal Parent (not services.exe): "+ apps[apps[pid][0]][1]+" ("+apps[pid][0]+") "+ "\n"   
+        elif process == "RuntimeBroker.exe":
+            for pid in pids:
+                if apps[pid][0] not in apps:
+                    analysis += "   Rough RuntimeBroker.exe process detected! Pid: "+pid+", None-Existing Parent: "+ apps[pid][0]+ "\n"
+                else:
+                    if apps[apps[pid][0]][1] != "svchost.exe":
+                        analysis += "   Rough RuntimeBroker.exe process detected! Pid: "+pid+", abnormal Parent (not svchost.exe): "+ apps[apps[pid][0]][1]+" ("+apps[pid][0]+") "+ "\n" 
+        elif process == "taskhostw.exe":
+            for pid in pids:
+                if apps[pid][0] not in apps:
+                    analysis += "   Rough taskhostw.exe process detected! Pid: "+pid+", None-Existing Parent: "+ apps[pid][0]+ "\n"
+                else:
+                    if apps[apps[pid][0]][1] != "svchost.exe":
+                        analysis += "   Rough taskhostw.exe process detected! Pid: "+pid+", abnormal Parent (not svchost.exe): "+ apps[apps[pid][0]][1]+" ("+apps[pid][0]+") "+ "\n" 
+        elif process == "winlogon.exe":
+            for pid in pids:
+                if apps[pid][0] in apps:
+                        analysis += "   Rough winlogon.exe process detected! Pid: "+pid+", winlogon.exe usualy has an exited parent ,abnormal Parent: "+ apps[apps[pid][0]][1]+" ("+apps[pid][0]+")\n"
+        elif process == "lsaiso.exe":
+            for pid in pids:
+                if apps[pid][0] not in apps:
+                    analysis += "   Rough lsaiso.exe process detected! Pid: "+pid+", None-Existing Parent: "+ apps[pid][0]+ "\n"
+                else:
+                    if apps[apps[pid][0]][1] != "wininit.exe":
+                        analysis += "   Rough lsaiso.exe process detected! Pid: "+pid+", abnormal Parent: "+ apps[apps[pid][0]][1]+ "\n"
+     if analysis.strip():  
+        lines = analysis.split('\n')  
+        lines.insert(0, "Process Masquerading detections (T1036):")  
+        return '\n'.join(lines)  
+     else:
+        return analysis  
+     return analysis                    
+    
+                        
+
+          
+          
 
 # def for powershell anomalies
-
+# ppidname = apps[apps[pid][0]][1]
 
 
 x = enumeratePslist()
+print ( DetectMasquerading(x))
 
-print (printhirerchy ('728',x))
+
         
 
